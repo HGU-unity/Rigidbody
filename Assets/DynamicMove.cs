423289d18256a43021f75f3a6a5bf9de787af2a3
Geoyeob Kim <gasbank@msn.com>
2018-02-06 11:15:19 +0900

    키보드 방향키로 Rigidbody (Dynamic) 움직이기

---
+++ Assets/DynamicMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+public class DynamicMove : MonoBehaviour {
+    public float speed = 5;
+    Rigidbody rb;
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate() {
+        var dx = Input.GetAxisRaw("Horizontal");
+        var dz = Input.GetAxisRaw("Vertical");
+        var dir = new Vector3(dx, 0, dz).normalized;
+        rb.velocity = dir * speed + rb.velocity.y * Vector3.up;
+        Debug.LogFormat("mag = {0}", rb.velocity.magnitude);
+    }
+}
